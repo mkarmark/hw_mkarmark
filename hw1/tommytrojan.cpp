917278733dc59afdde42af31fffa9d64a7d046cf
Mitali Karmarkar <mkarmark@usc.edu>
2016-01-17 14:57:23 -0800

    Problem6 1/17/16 2:57PM

--- hw1/tommytrojan.cpp
+++ hw1/tommytrojan.cpp
@@ -61,7 +61,7 @@ int main(int argc, char* argv[])
 		}
 		else {
 			if (trojans[i] == NULL) {
-				output << "Error -floor " << i << " is already empty" << endl;
+				output << "Error - floor " << i << " is already empty" << endl;
 			} else if (i < 0 || i >= floors) {
 				output << "Error - floor " << i << " does not exist" << endl;
 			} else {
@@ -76,13 +76,77 @@ int main(int argc, char* argv[])
 		}
 	  }
 	  else if (curr == "OBTAIN") {
-
+	  	int i, j, k;
+	  	ss >> i;
+	  	ss >> j;
+	  	ss >> k;
+	  	if (ss.fail()) {
+	  		output << "Error - incorrect command" << endl;
+	  	} else {
+	  		if (trojans[i] == NULL) {
+	  			output << "Error - floor" << i << " is already empty" << endl;
+	  		} else if (i < 0 || i >= floors) {
+	  			output << "Error - floor " << i << " does not exist" << endl;
+	  		} else if (j < 0 || j >= floorsizes[i]) {
+	  			output << "Error - student " << j << " does not exist on floor " << i << endl;
+	  		} else if (trojans[i][j] != NULL) {
+	  			output << "Error - student " << j << " on floor " << i << " already has possessions" << endl;
+	  		} else {
+	  			trojans[i][j] = new string[k+1];
+	  			for (int a = 0; a < k; a++) {
+	  				string n;
+	  				ss >> n;
+	  				if (ss.fail()) {
+	  					output << "Error - discrepancy between the number of possessions listed and the number of strings following" << endl;
+		  			} else {
+		  				trojans[i][j][a] = "";
+		  			}
+		  			trojans[i][j][a] = n;
+	  			}
+	  			trojans[i][j][k] == "";
+	  		}
+	  	}
 	  }
 	  else if (curr == "OUTPUT") {
-
+	  	int i, j;
+	  	ss >> i;
+		ss >> j;
+		if (ss.fail()) {
+			output << "Error - incorrect command" << endl;
+		}
+		else {
+			if (trojans[i] != NULL) {
+		  		output << "Error - floor " << i << " is not empty" << endl;
+		  	} else if (i < 0 || i >= floors) {
+		  		output << "Error - floor " << i << " does not exist" << endl;
+		  	} else if (j < 0 || j >= floorsizes[i]) {
+	  			output << "Error - student " << j << " does not exist on floor " << i << endl;
+		  	} else if (trojans[i][j] == NULL) {
+	  			output << "Error - student " << j << " on floor " << i << " has no possessions" << endl;
+	  		} else {
+	  			int k=0;
+	  			while (trojans[i][j][k] != "") {
+	  				output << trojans[i][j][k] << endl;
+	  				k++;
+	  			}
+	  		}
+		}
 	  }
 	  else {
-
+	  	for (int i=0; i<floors; i++) {
+	  		for (int j=0; j<floorsizes[i]; j++) {
+	  			if (trojans[i][j] != NULL) {
+	  				delete [] trojans[i][j];
+	  				trojans[i][j] = NULL;
+	  			}
+	  		}
+	  		if (trojans[i] != NULL) {
+	  			delete [] trojans[i];
+	  			trojans[i] = NULL;
+	  		}
+	  	}
+	  	delete [] trojans;
+	  	trojans = NULL;
 	  }
   }
   return 0;
