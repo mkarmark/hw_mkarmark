23e0c475b818b88666af38af0e8910d2c7b7c48d
Mitali Karmarkar <mkarmark@usc.edu>
2016-01-23 13:01:35 -0800

    Problem6 1/23/16 1:01PM

--- hw1/tommytrojan.cpp
+++ hw1/tommytrojan.cpp
@@ -4,6 +4,137 @@
 
 using namespace std;
 
+bool is_floor_valid(int floor_number, int floors, string ***trojans, bool need_floor_empty, ofstream &output) {
+	if (floor_number <= 0 || floor_number > floors) {
+		output << "Error - floor " << floor_number << " does not exist" << endl;
+		return false;
+	} else {
+		if (trojans[floor_number-1] != NULL && need_floor_empty) {
+			output << "Error - floor " << floor_number << " is not empty" << endl;
+			return false;
+		} else if (trojans[floor_number-1] == NULL && !need_floor_empty) {
+			output << "Error  floor " << floor_number << " is currently empty" << endl;
+			return false;
+		}
+	}
+	return true;
+}
+
+bool is_student_valid_on_floor(int student_number, int floor_number, int floors, int *floorsizes, string ***trojans, ofstream &output, bool need_possessions) {
+	if (is_floor_valid(floor_number, floors, trojans, false, output)) {
+		if (student_number <= 0 || student_number > floorsizes[floor_number-1]) {
+			output << "Error - student " << student_number << " does not exist on floor " << floor_number << endl;
+			return false;
+		} else if (trojans[floor_number-1][student_number-1] != NULL && !need_possessions) {
+			output << "Error - student " << student_number << " on floor " << floor_number << " already has possessions" << endl;
+			return false;
+		} else if (trojans[floor_number-1][student_number-1] == NULL && need_possessions) {
+			output << "Error - student " << student_number << " on floor " << floor_number << " does not have possessions" << endl;
+		}
+		return true;
+	} else {
+		return false;
+	}
+}
+
+void initialize_variables(string ***&trojans, int &floors, int *&floorsizes, int **&num_possessions, ifstream &input) {
+	input >> floors;
+  	trojans = new string**[floors];
+
+  	floorsizes = new int[floors];
+  	num_possessions = new int*[floors];
+
+  	for (int i = 0; i < floors; i++) {
+		floorsizes[i] = 0;
+	  	trojans[i] = NULL;
+	  	num_possessions[i] = NULL;
+  	}
+
+  	string garbageLine;
+  	getline(input, garbageLine);
+}
+
+void move_in(string ***&trojans, int floors, int **&num_possessions, int *&floorsizes, stringstream &ss, ofstream &output) {
+	int floor_number,num_students;
+	ss >> floor_number;
+	ss >> num_students;
+	string garbageVal;
+	if (ss.fail() || ss >> garbageVal) {
+		output << "Error - incorrect command" << endl;
+	} else {
+		if (is_floor_valid(floor_number, floors, trojans, true, output)) {
+		  	trojans[floor_number-1] = new string*[num_students];
+			num_possessions[floor_number-1] = new int[num_students];
+			for (int i=0; i<num_students; i++) {
+				trojans[floor_number-1][i] = NULL;
+				num_possessions[floor_number-1][i] = 0;
+			} 	
+			floorsizes[floor_number-1] = num_students;
+		}
+	}	  	 	
+}
+
+void move_out(string ***&trojans, int floors, int **&num_possessions, int *&floorsizes, stringstream &ss, ofstream &output) {
+	int floor_number;
+	ss >> floor_number;
+	string garbageVal;
+	if (ss.fail() || ss >> garbageVal) {
+		output << "Error - incorrect command" << endl;
+	} else {
+		if (is_floor_valid(floor_number, floors, trojans, false, output)) {
+			for (int i=0; i<floorsizes[floor_number-1]; i++) {
+				if (trojans[floor_number-1][i] != NULL) {
+					delete [] trojans[floor_number-1][i];
+					trojans[floor_number-1][i] = NULL;
+				}
+			} 
+			delete [] trojans[floor_number-1];
+			trojans[floor_number-1] = NULL;
+
+			delete [] num_possessions[floor_number-1];
+			num_possessions[floor_number-1] = NULL;
+
+			floorsizes[floor_number-1] - 0;	
+		}
+	}
+}
+
+void obtain_possessions(string ***&trojans, int floors, int **&num_possessions, int *&floorsizes, stringstream &ss, ofstream &output) {
+	int floor_number, student_number, possessions;
+	ss >> floor_number;
+	ss >> student_number;
+	ss >> possessions;
+	if (ss.fail()) {
+	  	output << "Error - incorrect command" << endl;
+	} else {
+	  	if (is_student_valid_on_floor(student_number, floor_number, floors, floorsizes, trojans, output, false)) {
+	  		trojans[floor_number-1][student_number-1] = new string[possessions];
+			num_possessions[floor_number-1][student_number-1] = possessions;
+			for (int i=0; i<possessions; i++) {
+				string n;
+				ss >> n;
+				trojans[floor_number-1][student_number-1][i] = n;
+			}	
+	  	}
+	}  			
+}
+
+void output_possessions(string ***&trojans, int floors, int **&num_possessions, int *&floorsizes, stringstream& ss, ofstream &output) {
+	int floor_number, student_number;
+	ss >> floor_number;
+	ss >> student_number;
+	string garbageVal;
+	if (ss.fail() || ss >> garbageVal) {
+		output << "Error - incorrect command" << endl;
+	} else {
+		if (is_student_valid_on_floor(student_number, floor_number, floors, floorsizes, trojans, output, true)) {
+	  		for (int i = 0; i < num_possessions[floor_number-1][student_number-1]; i++) {
+	  			output << trojans[floor_number-1][student_number-1][i] << endl;
+	  		}
+		}
+	}
+}
+
 void clear_building(int floors, int* &floorsizes, string ***&trojans, int **&num_possessions) {
 	for (int i=0; i<floors; i++) {
 	  	for (int j=0; j<floorsizes[i]; j++) {
@@ -28,42 +159,6 @@ void clear_building(int floors, int* &floorsizes, string ***&trojans, int **&num
 	num_possessions = NULL;
 }
 
-string is_floor_valid(int floor_number, int floors, string ***trojans, bool need_floor_empty) {
-	string return_string;
-	stringstream ss;
-	ss << "";
-	if (floor_number <= 0 || floor_number > floors) {
-		ss << "Error - floor " << floor_number << " does not exist" << endl;
-	} else {
-		if (trojans[floor_number-1] != NULL && need_floor_empty) {
-			ss << "Error - floor " << floor_number-1 << " is not empty" << endl;
-		} else if (trojans[floor_number-1] == NULL && !need_floor_empty) {
-			ss << "Error  floor " << floor_number << " is already empty" << endl;
-		}
-	}
-
-	ss >> return_string;
-	return return_string;
-}
-
-string is_student_valid_on_floor(int student_number, int floor_number, int floors, int *floorsizes, string ***trojans) {
-	string return_string;
-	stringstream ss;
-	ss << "";
-	string floor_is_invalid = is_floor_valid(floor_number, floors, trojans, true);
-	if (floor_is_invalid.compare("") != 0) {
-		return floor_is_invalid;
-	} else {
-		if (student_number <= 0 || student_number > floorsizes[floor_number-1]) {
-			ss << "Error - student " << student_number << " does not exist on floor " << floor_number << endl;
-		} else if (trojans[floor_number-1][student_number-1] != NULL) {
-			ss << "Error - student " << student_number << " on floor " << floor_number << " already has possessions" << endl;
-		}
-		ss >> return_string;
-		return return_string;
-	}
-}
-
 int main(int argc, char* argv[])
 {
   if(argc < 3){
@@ -79,117 +174,22 @@ int main(int argc, char* argv[])
   int **num_possessions;
   string curr;
 
-  input >> floors;
-  trojans = new string**[floors];
+  initialize_variables(trojans, floors, floorsizes, num_possessions, input);
 
-  //you will need to keep track of how many people are on each floor.
-  floorsizes = new int[floors];
-  num_possessions = new int*[floors];
-
-  for (int i = 0; i < floors; i++) {
-	  floorsizes[i] = 0;
-	  trojans[i] = NULL;
-	  num_possessions[i] = NULL;
-  }
-
-  string garbageLine;
-  getline(input, garbageLine);
-  //int count = 0;
   while(getline(input, curr)) {
 	  stringstream ss;
 	  ss << curr;
 	  ss >> curr;
+
 	  if (curr == "MOVEIN") {
-		  int i,k;
-		  ss >> i;
-		  ss >> k;
-		  string garbageVal;
-		  if (ss.fail() || ss >> garbageVal) {
-			output << "Error - incorrect command" << endl;
-		  } else {
-		  	string floor_is_valid = is_floor_valid(i, floors, trojans, true);
-		  	if (floor_is_valid.compare("") != 0) {
-		  		output << floor_is_valid << endl;
-		  	} else {
-		  	 	trojans[i-1] = new string*[k];
-		  	 	num_possessions[i-1] = new int[k];
-		  	 	for (int a = 0; a < k; a++) {
-		  	 		trojans[i-1][a] = NULL;
-		  	 		num_possessions[i-1][a] = 0;
-		  	 	}
-		  	 	floorsizes[i-1] = k;
-		  	 }
-		  }
-	  }
-	  else if (curr == "MOVEOUT") {
-		int i;
-		ss >> i;
-		string garbageVal;
-		if (ss.fail() || ss >> garbageVal) {
-			output << "Error - incorrect command" << endl;
-		} else {
-			string floor_is_valid = is_floor_valid(i, floors, trojans, false);
-			if (floor_is_valid.compare("") != 0) { 
-				output << floor_is_valid << endl; 
-			} else {
-				for (int a = 0; a < floorsizes[i]; a++) {
-					if (trojans[i-1][a] != NULL) {
-						delete [] trojans[i][a];
-						trojans[i-1][a] = NULL;
-					}
-				}
-				delete [] trojans[i-1];
-				trojans[i-1] = NULL;
-
-				delete [] num_possessions[i-1];
-				num_possessions[i-1] = NULL;
-
-				floorsizes[i] = 0;
-			}
-		}
-	  }
-	  else if (curr == "OBTAIN") {
-	  	int i, j, k;
-	  	ss >> i;
-	  	ss >> j;
-	  	ss >> k;
-	  	if (ss.fail()) {
-	  		output << "Error - incorrect command" << endl;
-	  	} else {
-	  		string student_is_valid_on_floor = is_student_valid_on_floor(j, i, floors, floorsizes, trojans);
-	  		if (student_is_valid_on_floor.compare("") != 0) {
-	  			output << student_is_valid_on_floor << endl;
-	  		} else {
-	  			trojans[i-1][j-1] = new string[k];
-	  			num_possessions[i-1][j-1] = k;
-	  			for (int a = 0; a < k; a++) {
-	  				string n;
-	  				ss >> n;
-		  			trojans[i-1][j-1][a] = n;
-	  			}
-	  		}
-	  	}
-	  }
-	  else if (curr == "OUTPUT") {
-	  	int i, j;
-	  	ss >> i;
-		ss >> j;
-		string garbageVal;
-		if (ss.fail() || ss >> garbageVal) {
-			output << "Error - incorrect command" << endl;
-		}
-		else {
-			string student_is_valid_on_floor = is_student_valid_on_floor(j, i, floors, floorsizes, trojans);
-			if (student_is_valid_on_floor.compare("") != 0) {
-				output << student_is_valid_on_floor << endl;
-			} else {
-	  			for (int a = 0; a < num_possessions[i-1][j-1]; a++) {
-	  				output << trojans[i-1][j-1][a] << endl;
-	  			}
-			}
-	  	}
-	  }	
-	  else {
+		move_in(trojans, floors, num_possessions, floorsizes, ss, output);
+	  } else if (curr == "MOVEOUT") {
+		move_out(trojans, floors, num_possessions, floorsizes, ss, output);
+	  } else if (curr == "OBTAIN") {
+	  	obtain_possessions(trojans, floors, num_possessions, floorsizes, ss, output);
+	  } else if (curr == "OUTPUT") {
+	  	output_possessions(trojans, floors, num_possessions, floorsizes, ss, output);
+	  }	else if (curr != "") {
 	  	output << "Error - line does not begin with one of four commands: MOVEIN, MOVEOUT, OBTAIN, OUTPUT" << endl;
 	  }
   }
